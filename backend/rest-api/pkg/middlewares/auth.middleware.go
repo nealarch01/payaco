3b4625656739987671fdb0a9cd241419f3e329e2
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-11-25 12:16:37 -0800

    Fix content-type header

--- backend/rest-api/pkg/middlewares/auth.middleware.go
+++ backend/rest-api/pkg/middlewares/auth.middleware.go
@@ -2,8 +2,9 @@ package middlewares
 
 import (
 	"fmt"
-	"github.com/nealarch01/payaco/pkg/authentication"
 	"net/http"
+
+	"github.com/nealarch01/payaco/pkg/authentication"
 )
 
 // Auth Middlware, checks if access token is valid
@@ -11,10 +12,10 @@ func AuthenticationTokenValid(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Get the token from the header
 		token := r.Header.Get("Authorization")
+		w.Header().Set("Content-Type", "application/json")
 		if token == "" {
 			fmt.Println("No token provided")
 			w.WriteHeader(http.StatusUnauthorized)
-			w.Header().Set("Content-Type", "application/json")
 			w.Write([]byte(`{"message": "Token was not provided"}`))
 			return
 		}
@@ -23,7 +24,6 @@ func AuthenticationTokenValid(next http.Handler) http.Handler {
 			fmt.Println("Invalid token")
 			w.WriteHeader(http.StatusUnauthorized)
 			// Return JSON response
-			w.Header().Set("Content-Type", "application/json")
 			w.Write([]byte(`{"message": "This token is invalid"}`))
 			return
 		}
